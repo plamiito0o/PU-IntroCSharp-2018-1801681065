1403939fcf34205fa85500924e451210f479dc90
plamiito0o <plamiito0o@gmail.com>
2018-10-21 11:33:56 +0300

    Included task 1,2,3 for week 3.

---
+++ PUIntroHomeworks/BusTicketLuckyNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BusTicketLuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luckyTicketsCount = 0;
+            int unluckyTicketsCount = 0;
+
+            /* Create a for loop, which will check if the sum of the first 3 digits
+               is equal to the sum of the last 3 digits. If the sums are equal, the
+               ticket is lucky and we'll display. If not, it is considered an unlucky ticket. */
+            for (int i = 0; i < 1000000; i++)
+            {
+                if ((i / 100000 + i % 100000 / 10000 + i % 100000 % 10000 / 1000) == (i % 100000 % 10000 % 1000 / 100 + i % 100000 % 10000 % 1000 % 100 / 10 + i % 10))
+                {
+                    Console.WriteLine(i);
+                    luckyTicketsCount++;
+                }
+                else
+                {
+                    unluckyTicketsCount++;
+                }
+            }
+
+            // Finally, display the number of lucky and unlucky tickets and their sum.
+            Console.WriteLine("The number of lucky tickets is: " + luckyTicketsCount);
+            Console.WriteLine("The number of unlucky tickets is: " + unluckyTicketsCount);
+            Console.WriteLine($"Total tickets: {luckyTicketsCount + unluckyTicketsCount}");
+            Console.ReadKey();
+        }
+   
+    }
+}
