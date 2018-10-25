439e4031ac188346e69783e668f62c89d935b50e
plamiito0o <plamiito0o@gmail.com>
2018-10-25 22:17:10 +0300

    Included task 1,2 for week 4

---
+++ PUIntroHomeworks/WorkingWithSomeIntegers/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace WorkingWithSomeIntegers
+{
+    class Program
+    {
+        static Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[100];
+
+            // Generate 100 random numbers and store them in the array
+            Console.WriteLine("Here are 100 randomly generated numbers in the interval between 0 and 132.");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = rand.Next(132);
+                Console.WriteLine(intArray[i]);
+            }
+
+            /* Display the options and then using a switch-case, we'll output
+               different results, according to the chosen option */
+            Console.WriteLine("Choose what you want to do with them (type in the letter): ");
+            Console.WriteLine("a) Display the numbers that have even indexes.");
+            Console.WriteLine("b) Display the numbers that are odd and have odd indexes.");
+            Console.WriteLine("c) Display the numbers that are divided by 3 without a remainder.");
+            Console.WriteLine("d) Display the numbers that are divided by 7 and have 1 as remainder.");
+            Console.WriteLine("e) Display the numbers that are in the interval between 26 and 100.");
+            Console.WriteLine("f) Display the numbers that are not in the interval between 26 and 100.");
+            Console.WriteLine("----------------------------------------------------------");
+
+            char option = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("----------------------------------------------------------");
+
+            switch (option)
+            {
+                case 'a':
+                    for (int i = 0; i < intArray.Length; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            /* Don't forget that array indexes start from 0. If you
+                               want to make it more understandable (index starts from 1), 
+                               instead of i, type in i + 1 after "has index". */
+                            Console.WriteLine($"{intArray[i]} has index {i}.");
+                        }
+                    }
+                    break;
+                case 'b':
+                    for (int i = 0; i < intArray.Length; i++)
+                    {
+                        if ((intArray[i] % 2) != 0 && (i % 2 != 0))
+                            Console.WriteLine($"{intArray[i]} is odd and has index {i}.");
+                    }
+                    break;
+                case 'c':
+                    for (int i = 0; i < intArray.Length; i++)
+                    {
+                        if (intArray[i] % 3 == 0)
+                            Console.WriteLine($"{intArray[i]} is divided by 3 without a remainder.");
+                    }
+                    break;
+                case 'd':
+                    for (int i = 0; i < intArray.Length; i++)
+                    {
+                        if (intArray[i] % 7 == 1)
+                            Console.WriteLine($"{intArray[i]} is divided by 7 with a remainder of 1.");
+                    }
+                    break;
+                case 'e':
+                    for (int i = 0; i < intArray.Length; i++)
+                    {
+                        if ((intArray[i] >= 26) && (intArray[i] <= 100))
+                            Console.WriteLine($"{intArray[i]} is between 26 and 100.");
+                    }
+                    break;
+                case 'f':
+                    for (int i = 0; i < intArray.Length; i++)
+                    {
+                        if ((intArray[i] < 26) || (intArray[i] > 100))
+                            Console.WriteLine($"{intArray[i]} is not between 26 and 100.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Incorrect option!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PUIntroHomeworks/WorkingWithSomeLetters/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+
+namespace WorkingWithSomeLetters
+{
+    class Program
+    {
+        static Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            //Console.OutputEncoding = encoding.UTF8;
+
+            // Declare and initialize a string array which will store 100 letters (chars)
+            string[] stringArray = new string[100];
+
+            Console.WriteLine("Даден е низ от 100 случайни букви на кирилица. Програмата показва кои гласни са широки, тесни и съставни, както и кои съгласни са звучни, беззвучни и сонорни.");
+            Console.WriteLine("-------------------------------------------------------");
+
+            // Generate 100 letters using the rand variable
+            for (int i = 0; i < 100; i++)
+            {
+                // Notice how we generate random letters using their UTF-8 codes (capital letters not included)
+                stringArray[i] = ((char)rand.Next(1072, 1103)).ToString();
+                Console.Write(stringArray[i]);
+            }
+
+            Console.WriteLine("\n-------------------------------------------------------");
+
+            // Depending what type the letter is, it wall fall in its corresponding category
+            for (int i = 0; i < 100; i++)
+            {
+                if ((stringArray[i] == "а") || (stringArray[i] == "o") || (stringArray[i] == "е"))
+                {
+                    Console.WriteLine($"Буквата ({stringArray[i]}) е широка гласна и се намира под номер {i}.");
+                }
+                if ((stringArray[i] == "и") || (stringArray[i] == "у") || (stringArray[i] == "ъ"))
+                {
+                    Console.WriteLine($"Буквата ({stringArray[i]}) e тясна гласна и се намира под номер {i}.");
+                }
+                if ((stringArray[i] == "ю") || (stringArray[i] == "я"))
+                {
+                    Console.WriteLine($"Буквата ({stringArray[i]}) е съставна гласна и се намира под номер {i}.");
+                }
+                if ((stringArray[i] == "п") || (stringArray[i] == "ф") || (stringArray[i] == "к") || (stringArray[i] == "т") || (stringArray[i] == "ш") || (stringArray[i] == "с") || (stringArray[i] == "x") || (stringArray[i] == "ц"))
+                {
+                    Console.WriteLine($"Буквата ({stringArray[i]}) е беззвучна съгласна и се намира под номер {i}.");
+                }
+                if ((stringArray[i] == "л") || (stringArray[i] == "м") || (stringArray[i] == "н") || (stringArray[i] == "р"))
+                {
+                    Console.WriteLine($"Буквата ({stringArray[i]}) е сонорна съгласна и се намира под номер {i}.");
+                }
+                if ((stringArray[i] == "б") || (stringArray[i] == "в") || (stringArray[i] == "г") || (stringArray[i] == "д") || (stringArray[i] == "ж") || (stringArray[i] == "з") || (stringArray[i] == "ч"))
+                {
+                    Console.WriteLine($"Буквата ({stringArray[i]}) е звучна съгласна и се намира под номер {i}.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
