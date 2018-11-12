7692c9b370121e52a2196aa5ace4e5056f85a4dc
plamiito0o <plamiito0o@gmail.com>
2018-11-13 01:32:40 +0200

    Some changes

--- PUIntroHomeworks/PrintSmileyAfterName/Program.cs
+++ PUIntroHomeworks/PrintSmileyAfterName/Program.cs
@@ -5,33 +5,50 @@ namespace PrintSmileyAfterName
 {
     class Program
     {
-        private static int i;
 
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            string[] names = System.IO.File.ReadAllLines(@"C:\Users\plami\Desktop\Work_Folder\PU-IntroCSharp-2018-1801681065\PUIntroHomeworks\PrintSmileyAfterName\files\students.txt");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\plami\Desktop\Work_Folder\PU-IntroCSharp-2018-1801681065\PUIntroHomeworks\PrintSmileyAfterName\files\students.txt");
             System.Console.WriteLine("Contents of students.txt=");
-                      
-                //Console.WriteLine("\t" + line);
-                /*int sum = 0;
-                for (int i = 0; i < names.Length; i++)
-                {
-                    sum += (int)names[i];
-                  
-                }
-               if (sum >15000)
-            { 
-                PrintSmileys(names);
-            }*/
-           // }
-            PrintSmileys(names);
-           
-             void PrintSmileys(string[] words)
+            int SmileyStudents = 0;
+            foreach (string line in lines)
             {
-                for (int i = 0; i < words.Length; i++)
-                { Console.WriteLine($"Name:{words[i]}{(char)9786}"); }
+                string[] elements = line.Split(' ');
+                string FirstName = elements[1];
+                string SecondName = elements[2];
+                string ThirdName = elements[3];
+                string name = $"{FirstName} {SecondName} {ThirdName}";
+                int sum1 = 0;
+                int sum2 = 0;
+                int sum3 = 0;
+                int sum = 0;
+                for(int i=0;i<FirstName.Length;i++)
+                {
+                    sum1 = sum1 + (int)FirstName[i];
+                }
+                for(int i=0;i<SecondName.Length;i++)
+                {
+                    sum2 = sum2 + (int)SecondName[i];
+                }
+                for(int i=0;i<ThirdName.Length;i++)
+                {
+                    sum3 = sum3 + (int)ThirdName[i];
+                }
+                sum = sum1 + sum2 + sum3;
+
             }
+            if (sum > 25000)
+            {
+                Console.WriteLine($"{FirstName} {SecondName} {ThirdName} {(char)9786}");
+                SmileyStudents = 1;
+            }
+
+            Console.WriteLine($"TOTAL LINES: {lines.Length}");
+            Console.WriteLine($"Total smiley students: {SmileyStudents}");
+            
+           
+            
             Console.ReadKey();
         }
     }
